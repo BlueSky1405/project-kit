2e44a4bf7ef673e4119bd74493a37946c9dae840
yxn <xiaonan.yang@ppiaas.cn>
2022-10-19 10:14:42 +0800

    fix mysql add migrate

---
+++ database/mysql/migrate.go
@@ -0,0 +1,27 @@
+package mysql
+
+import "fmt"
+
+func InitMigrateCmd(config *Config, do string, tables ...interface{}) {
+	migrateORM, cleanup := NewMigrateConn(config)
+	defer cleanup()
+	var orm *Conn
+	if do == "migrate" {
+		tmpORM, cleanup2 := NewConn(config)
+		orm = tmpORM
+		defer cleanup2()
+	}
+
+	switch do {
+	case "create":
+		migrateORM.CreateDB()
+	case "drop":
+		migrateORM.DropDB()
+	case "migrate":
+		if err := orm.GetDB().AutoMigrate(tables...); err != nil {
+			panic(err)
+		}
+	default:
+		panic(fmt.Sprintf("no support do:%s", do))
+	}
+}
--- database/mysql/mysql.go
+++ database/mysql/mysql.go
@@ -31,11 +31,27 @@ type Conn struct {
 	conn *gorm.DB
 }
 
-// NewConn 初始化mysql连接对象, func() - 关闭方法
+func (c *Config) dialector(initDB bool) gorm.Dialector {
+	if initDB {
+		return mysql.Open(fmt.Sprintf("%s:%s@tcp(%s)/",
+			c.Username, c.Password, c.Url))
+	}
+	// mysql: url 示例: 127.0.0.1:3306(包含端口)
+	return mysql.Open(fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		c.Username, c.Password, c.Url, c.DatabaseName))
+}
+
 func NewConn(cfg *Config) (*Conn, func()) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", cfg.Username, cfg.Password, cfg.Url, cfg.DatabaseName)
-	dialector := mysql.Open(dsn)
-	db, err := gorm.Open(dialector)
+	return newConn(cfg, false)
+}
+
+func NewMigrateConn(cfg *Config) (*Conn, func()) {
+	return newConn(cfg, true)
+}
+
+// NewConn 初始化mysql连接对象, func() - 关闭方法
+func newConn(cfg *Config, initDb bool) (*Conn, func()) {
+	db, err := gorm.Open(cfg.dialector(initDb))
 	if err != nil {
 		panic(any(err))
 	}
@@ -56,4 +72,22 @@ func NewConn(cfg *Config) (*Conn, func()) {
 
 func (c *Conn) GetDB() *gorm.DB {
 	return c.conn
+}
+
+func (c *Conn) CreateDB() {
+	createDbSQL := "CREATE DATABASE IF NOT EXISTS " + c.cfg.DatabaseName + " DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;"
+	err := c.GetDB().Exec(createDbSQL).Error
+	if err != nil {
+		panic("创建失败：" + err.Error() + " sql:" + createDbSQL)
+	}
+	fmt.Println(fmt.Sprintf("database: %v create success", c.cfg.DatabaseName))
+}
+
+func (c *Conn) DropDB() {
+	dropDbSQL := "DROP DATABASE IF EXISTS " + c.cfg.DatabaseName + ";"
+	err := c.GetDB().Exec(dropDbSQL).Error
+	if err != nil {
+		panic("删除失败：" + err.Error() + " sql:" + dropDbSQL)
+	}
+	fmt.Println(fmt.Sprintf("database: %v drop success", c.cfg.DatabaseName))
 }
